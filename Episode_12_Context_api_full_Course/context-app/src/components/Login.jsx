b0ff435563aed34e5a147b39fdaff58c25048a4a
programmertarini <tariniprasad556@gmail.com>
2025-05-16 13:21:59 +0530

    Login using context api

---
+++ Episode_12_Context_api_full_Course/context-app/src/components/Login.jsx
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useState } from "react";
+import UserContext from "../context/UserContext";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const {setUser} = useContext(UserContext);
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    setUser({username , password})
+
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value)
+        }}
+        type="text"
+        placeholder="username"
+      />
+
+      {" "}
+      <input
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value)
+        }}
+        type="password"
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>submit</button>
+    </div>
+  );
+};
+
+export default Login;
